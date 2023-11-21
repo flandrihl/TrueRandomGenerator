46541622567925e4dcd94d5c28ed65f5ace64f69
Konopelko <konopelko.an@gmail.com>
2023-11-21 15:38:20 +0300

    Create
    Create repository

---
+++ Random.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace TrueRandomGenerator
+{
+    public  class Random
+    {
+        public static int Next()
+        {
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomNumber = new byte[4];
+                rng.GetBytes(randomNumber);
+                int trueRandomNumber = BitConverter.ToInt32(randomNumber, 0);
+
+                return trueRandomNumber;
+            }
+        }
+    }
+}
